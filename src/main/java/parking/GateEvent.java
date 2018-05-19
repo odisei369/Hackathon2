f56dc097caa11e60d25424ee25f53fb7d3901d8d
odisei369 <petrenko.ilia@gmail.com>
2018-05-19 23:06:13 +0200

    fifo added

--- src/main/java/parking/GateEvent.java
+++ src/main/java/parking/GateEvent.java
@@ -1,5 +1,33 @@
 package parking;
 
 public class GateEvent extends HackathonEvent{
+    Route route;
+    boolean loading;
+    Gate gate;
 
+    public String getType(){
+        return "GateEvent";
+    }
+
+    public int getRoute() {
+		return route;
+	}
+	public void setRoute(Route route) {
+		this.route = route;
+    }
+    public int getLoading() {
+		return loading;
+    }
+    public int getUnloading() {
+		return !loading;
+	}
+	public void setLoading(boolean loading) {
+		this.loading = loading;
+    }
+    public int getGate() {
+		return gate;
+	}
+	public void setGate(Gate gate) {
+		this.gate = gate;
+    }
 }
