b8a2f82ed3b23a2fc721b81f0ccef6acd1eecfa4
Franek Matera <franek.matera@gmai.com>
2018-05-19 23:24:44 +0200

    no compilation errors

--- src/main/java/parking/RouteEvent.java
+++ src/main/java/parking/RouteEvent.java
@@ -2,9 +2,31 @@ package parking;
 
 public class RouteEvent extends HackathonEvent {
 
-	public Vehicle Vehicle ;
-	private int Length;
-	private GateEvent Gate;
+	private GateEvent gateEvent;
+	
+	public RouteEvent() {
+		
+	}
+	
+	public RouteEvent(int length) {
+		setDuration(length);
+	}
+	
+	@Override
+	public String getType() {
+
+		return "RouteEvent";
+	}
+	
+	public GateEvent getGateEvent() {
+		return gateEvent;
+	}
+
+	public void setGateEvent(GateEvent gateEvent) {
+		this.gateEvent = gateEvent;
+	}
+	
+	
 	
 }
   
