5d31c628be62261f3a016f85da98ce037ee2bc81
Franek Matera <franek.matera@gmai.com>
2018-05-19 22:36:45 +0200

    implemented class HackatonEvent

--- src/main/java/parking/HackathonEvent.java
+++ src/main/java/parking/HackathonEvent.java
@@ -2,4 +2,34 @@ package parking;
 
 public abstract class HackathonEvent {
 
+	private int start;
+	private int duration;
+	abstract public String getType();
+	private Vehicle vehicle;
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+	
+	public int getEnd() {
+		return start + duration;
+	}
+	
+	public void elongate(int howMuch) {
+		duration += howMuch;
+	}
 }
