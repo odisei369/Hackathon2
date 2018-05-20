6d8bc0534877324960d14bd114af97afd06b7416
odisei369 <petrenko.ilia@gmail.com>
2018-05-20 09:42:49 +0200

    thread sleep added

--- src/main/java/parking/GreetingController.java
+++ src/main/java/parking/GreetingController.java
@@ -15,6 +15,7 @@ public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(500); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 }
